9632e99da351b7480aa9620e5371cc2c35e96acc
dark00shadow <twistcharlievlogs@gmail.com>
2020-07-20 18:42:34 +0100

    changes

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from pyglet.window import key
 from pyglet.gl import glEnable,glBlendFunc,GL_BLEND,GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA
 window = pyglet.window.Window(caption='shooting' ,width=600,height=600)
 window.set_location(window.screen.width//2-window.width//2, window.screen.height//2-window.height//2)
-window.set_icon(pyglet.resource.image('Icon.ico'))
+window.set_icon(pyglet.image.load('textures/icon.ico'))
 
 glEnable(GL_BLEND)
 glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
@@ -14,9 +14,9 @@ window.push_handlers(KeyHandler)
 ObjectBatch = pyglet.graphics.Batch()
 
 
-Player = pyglet.sprite.Sprite(pyglet.resource.image('player/Player_direction0.png') ,x=300,y=300 ,batch=ObjectBatch)
+Player = pyglet.sprite.Sprite(pyglet.image.load('textures/player/Player_direction0.png') ,x=300,y=300 ,batch=ObjectBatch)
 PlayerDirection = 'up'
-Bullet = pyglet.sprite.Sprite(pyglet.resource.image('bullet/bullet1.png') ,x=300,y=300 ,batch=ObjectBatch)
+Bullet = pyglet.sprite.Sprite(pyglet.image.load('textures/bullet/bullet1.png') ,x=300,y=300 ,batch=ObjectBatch)
 BulletDirection = 'up'
 Bullet.visible = False
 SpacePressed = False
@@ -24,16 +24,16 @@ shoot = False
 def update(dt):
     global SpacePressed, PlayerDirection, BulletDirection, shoot
     if KeyHandler[key.W]:
-        Player.image =pyglet.resource.image('player/Player_direction0.png')
+        PlayerDirection = 'up'
         Player.y += 1
     if KeyHandler[key.S]:
-        Player.image = pyglet.resource.image('player/Player_direction2.png')
+        PlayerDirection = 'down'
         Player.y -= 1
     if KeyHandler[key.A]:
-        Player.image = pyglet.resource.image('player/Player_direction1.png')
+        PlayerDirection = 'left'
         Player.x -= 1
     if KeyHandler[key.D]:
-        Player.image = pyglet.resource.image('player/Player_direction3.png')
+        PlayerDirection = 'right'
         Player.x += 1
     if SpacePressed == False and KeyHandler[key.SPACE] and Bullet.visible == False and shoot == True:
         SpacePressed = True
@@ -43,10 +43,10 @@ def update(dt):
     if SpacePressed == True and not KeyHandler[key.SPACE]: SpacePressed = False
 
     # Player flip
-    if Player.image == pyglet.resource.image('player/Player_direction0.png'): PlayerDirection = 'up'
-    if Player.image == pyglet.resource.image('player/Player_direction2.png'): PlayerDirection = 'down'
-    if Player.image == pyglet.resource.image('player/Player_direction1.png'): PlayerDirection = 'left'
-    if Player.image == pyglet.resource.image('player/Player_direction3.png'): PlayerDirection = 'right'
+    if PlayerDirection == 'up': Player.image = pyglet.image.load('textures/player/Player_direction0.png')
+    if PlayerDirection == 'down': Player.image = pyglet.image.load('textures/player/Player_direction2.png')
+    if PlayerDirection == 'left': Player.image = pyglet.image.load('textures/player/Player_direction1.png')
+    if PlayerDirection == 'right': Player.image = pyglet.image.load('textures/player/Player_direction3.png')
 
     # Pos of bullet
     if not Bullet.visible and PlayerDirection == 'up':
@@ -68,9 +68,9 @@ def update(dt):
 
     # Bullet direction
     if BulletDirection == 'up' or BulletDirection == 'down':
-        Bullet.image = pyglet.resource.image('bullet/bullet1.png')
+        Bullet.image = pyglet.image.load('textures/bullet/bullet1.png')
     if BulletDirection == 'left' or BulletDirection == 'right':
-        Bullet.image = pyglet.resource.image('bullet/bullet0.png')
+        Bullet.image = pyglet.image.load('textures/bullet/bullet0.png')
 
     # Bullet movment
     if BulletDirection == 'up' and Bullet.visible == True: Bullet.y +=4
@@ -83,7 +83,6 @@ def update(dt):
     if Bullet.x <= 5 and BulletDirection == 'left': Bullet.visible = False
     if Bullet.y >= 590 and BulletDirection == 'up': Bullet.visible = False
     if Bullet.y <= 5 and BulletDirection == 'down': Bullet.visible = False
-
 @window.event()
 def on_draw():
     window.clear()
